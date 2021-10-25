1633bbd0d442636573ae3e4063650216ee6a3698
Dragoș-Andrei Dumitraș <59060738+Dragos-Andrei-75@users.noreply.github.com>
2021-10-25 13:52:13 +0300

    Add Project

---
+++ Assets/Scripts/MouseLook.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections; //This is a library. A library is a collection of predefined classes, methods, variables, etc.
+using System.Collections.Generic;
+using UnityEngine;
+
+//access_modifier type variable = value;
+//access_modifier type variable = class.variable;
+//access_modifier type variable = class.method(type variable_2, type variable_3, ...);
+
+public class MouseLook : MonoBehaviour //This is a class. A class is a container that includes methods and variables.
+{                                      //In Unity, the main class derives from the built-in class "MonoBehaviour".
+                                       //The main class must have the same name as the script file it belongs to in order for the script to work properly.
+    
+    public Transform playerBody; //This is a variable. A variable is a container that holds data or the memory adress of data.
+                                 //"public" highlights the fact that this variable is visible to other scripts aswell as in the Inspector.
+                                 //"Transform" is a reference data type. This variable will be used to refer to the "Transform" component of the GameObject to which this script is assigned.
+    
+    private float xRotation = 0f; //"private" highlights the fact that this variable is invisible to other scripts as well as in the Inspector.
+    
+    [SerializeField] private float mouseSensitivity = 500f; //"[SerializeField]" will make this variable visible in the Inspector in spite of the fact that it is declared as a private variable, without
+                                                            //giving users the option to modify it.
+
+    //Start is called before the first frame update
+    void Start() //This is a method. A method is a container that includes instructions and variables.
+    {
+        Cursor.lockState = CursorLockMode.Locked; //"Cursor" is a cursor API for setting the mouse pointer.
+                                                  //"lockState" determines wether the hardware pointer is locked to the center of the view, constrained to the game window or not constrained at all.
+                                                  //"CursorLockMode" is used to describe how the cursor should behave(Locked, Constrained or None).
+                                                  //This line of code will position the cursor at the center of the screen and make it invisible.
+    }
+
+    //Update is called once per frame
+    void Update()
+    {
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime; //"Input" is an interface into the Input system.
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime; //"GetAxis" returns the value of the specified virtual axis.
+                                                                                     //"Mouse X" / "Mouse Y" is a predefined virtual axis in Unity that is going to change based on the mouse movement.
+                                                                                     //"Time" is an interface used to get time information from Unity.
+                                                                                     //"deltaTime" provides the time between the current frame and the previous frame.
+
+        playerBody.Rotate(Vector3.up * mouseX); //"Rotate" rotates GameObjects.
+                                                //"Vector3" is a representation of 3D vectors and points that passes 3D positions and directions around.
+                                                //This line of code will allow the user to move the camera from side to side(along the X axis). Moving the camera along the X axis means rotating the
+                                                //camera around the Y axis.
+
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f); //"Mathf" is a collection of common math methods.
+                                                       //"Clamp" will limit the rotation around the X axis to 180 degrees in order to prevent the player from over-rotating the camera.
+        
+        transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f); //"transform" refers to the "Transform" component of the GameObject to which this script is assigned.
+                                                                       //"Quaternion" is used to represent rotations.
+                                                                       //"Euler" returns a rotation around the X, Y and Z axis in that specific order.
+                                                                       //This line of code will allow the user to move the camera up and down(along the Y axis). Moving the camera along the Y axis
+                                                                       //means rotating the camera around the X axis.
+    }
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public CharacterController controller;
+    public float speed = 15f;
+
+    Vector3 velocity;
+    [SerializeField] private float gravity = -9.81f;
+
+    private bool isGrounded;
+    public Transform groundCheck; //position of the sphere
+    private float groundDistance = 0.5f; //radius of the sphere
+    public LayerMask groundMask;
+
+    public float jumpHeight = 3f;
+
+    private void Update()
+    {
+        float moveVertical = Input.GetAxis("Vertical"); //Vertical movement takes place on the Z axis.
+        float moveHorizontal = Input.GetAxis("Horizontal"); //Horizontal movement takes place on the X axis.
+
+        //Vector3 move = new Vector3(moveHorizontal, 0f, moveVertical); //This technique is not convenient for a first person perspective because it uses global coordinates.
+        Vector3 move = transform.forward * moveVertical + transform.right * moveHorizontal; //"forward" and "right" are normalized vectors representing the Z and X axis respectively.
+        controller.Move(move * speed * Time.deltaTime); //"Move" is a more complex move method that takes absolute movement deltas.
+
+        velocity.y += gravity * Time.deltaTime;
+        controller.Move(velocity * Time.deltaTime);
+
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask); //"Physics" contains global physics properties and helper methods.
+                                                                                            //"CheckSphere" returns "true" if there are any colliders overlapping the sphere defined by
+        if (isGrounded && velocity.y < 0)                                                   //position(groundCheck.position) and radius(groundDistance) in world coordinates.
+        {
+            velocity.y = -2f;
+        }
+
+        if (isGrounded && Input.GetButtonDown("Jump")) //"GetButtonDown" is a function that returns "true" when the user presses down the specified virtual button.
+        {
+            velocity.y = Mathf.Sqrt(jumpHeight * -2 * gravity); //"Sqrt" is a function that works as a square root.
+        }
+    }
+}
